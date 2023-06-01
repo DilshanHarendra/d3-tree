a9f873d8913acaf6c7fd9655ebadb39363605b0f
DilshanHarendra <harendracripto@gmail.com>
2023-06-01 11:59:31 +0530

    modify tree

--- main.js
+++ main.js
@@ -17,14 +17,10 @@ console.log(jsondata)
 
 
 let mouseX = 0;
-//these global variables I should later get via closure
-let buttonTracker = [];
 let rootNode = d3.hierarchy(jsondata, d=>d.children);
 var pathLinks = rootNode.links();
-var updatePathLinks;
 
-var circleLinks = rootNode.descendants();
-var updateCircleLinks;
+
 
 var textLinks = rootNode.descendants();
 var updateTextLinks;
@@ -45,19 +41,17 @@ document.querySelector("#chart").classList.add("center");
 
 //let rootNode = d3.hierarchy(data);
 
-
+    const nodeWidth = 100, nodeHeight=30;
 
 
 let g = svg.append('g')
             .attr('transform', 'translate(140,50)');
 
-    let layout = d3.tree().size([dim.height-50, dim.width-320]);
+    let layout = d3.tree().size([dim.height-50, dim.width-320])
+        .separation(function(a, b) { return (a.parent == b.parent ? nodeHeight*2 : nodeHeight*3); });
 
     layout(rootNode);
-    console.log(rootNode.links());
-    console.log("----------------------");
-    console.log(rootNode.descendants());
-    //console.log(rootNode.descendants());
+
    //first lets create a path
    let lines = g.selectAll('path');
 
@@ -72,31 +66,20 @@ let group =  g.selectAll('path')
         return enter.append('path')
                     .attrs({
                         'd': d3.linkHorizontal()
-                        .x(d => mouseX)
+                        .x(d => d.y)
                          .y(d => d.x),
                      'fill':'none',
                      'stroke':'white'
                     })
+      //      .attr('transform', `translate(${nodeWidth},0)`);
+
     },
     function(update){
         return update;
     },
-    function(exit){
-
-
-
-        return exit.call(path => path.transition().duration(300).remove()
-                                                .attr('d', d3.linkHorizontal()
-                                                              .x(d => mouseX)
-                                                              .y(d =>d.x)));
-    }
-
 
 )
-.call(path => path.transition().duration(1000).attr('d', d3.linkHorizontal()
-        .x(d => d.y)
-         .y(d => d.x))
-         .attr("id", function(d,i){return "path"+i}));
+
 
 
 }
@@ -104,16 +87,17 @@ update(pathLinks); //rootNode.links()
 
 
 
-function updateCircles(data){
-    g.selectAll('circle')
+const drawNode =(data)=>{
+  g.selectAll('rect')
     .data(data, (d) => d.data.name)
-    .join(
-        function(enter){
-            return enter.append('circle')
+    .join((enter)=>{
+            return enter.append('rect')
                         .attrs({
-                            'cx':(d)=> mouseX,
-                            'cy':(d) => d.x,
-                            'r':12,
+                            'x':(d)=> d.y,
+                            'y':(d) => d.x-nodeHeight/2,
+                            'width':nodeWidth,
+                            'height':nodeHeight,
+                            // 'r':12,
                             'fill':(d) => {
                                 if(d.children == undefined){
                                     return 'red'
@@ -121,141 +105,140 @@ function updateCircles(data){
                                 return 'green'
                             },
                             'id': (d,i) =>d.data.name,
-                            'class':'sel'
+                            'class':'node'
+
                         })
+
         },
         function(update){
             return update;
         },
         function(exit){
 
-            return exit.call(path => path.transition().duration(300).remove()
-            .attrs({
-                'cx':(d) =>mouseX,
-                'r':(d) => 0
-            }));
+            // return exit.call(path => path.transition().duration(300).remove()
+            // .attrs({
+            // //    'cx':(d) =>mouseX,
+            //     'r':(d) => 0
+            // }));
 
         }
+        )
 
+ //   .call(circle => circle.transition().duration(1000).attr('cx', (d)=>d.y))
 
-    )
-    .call(circle => circle.transition().duration(1000).attr('cx', (d)=>d.y))
-
-    .on('mouseover', function(d){
-
-       d3.select(this)
-           .attrs({
-               'fill':'orange',
-
-           })
-           .transition().duration(100).attr('r', 16);
-    })
-    .on('mouseout', function(d){
-       d3.select(this)
-           .attr('fill', (d)=>{
-                if(d.children ==undefined){
-                    return 'red'
-                }
-                return 'green'
-           })
-           .transition().duration(100).attr('r', 12)
-
-    })
-    .on('click', async function(d){
-
-           let buttonId = d3.select(this)["_groups"][0][0]["attributes"].id.value;
-           mouseX = d3.select(this)["_groups"][0][0]["attributes"].cx.value;
-           //check to see if button already exists aka has been clicked
-           //if it does, we need to send that data to update function
-           //and remove that object
-
-           let checkButtonExists = buttonTracker.filter(button => button.buttonId == buttonId);
-
-           if(checkButtonExists[0]!=undefined){
-                //also remove this item from button tracker
-               buttonTracker = buttonTracker.filter(button => button.buttonId != buttonId);
-
-               //handle path update
-               pathLinks = checkButtonExists[0].buttonPathData.concat(pathLinks);
-
-               update(pathLinks);
-
-
-                //handle  circle update
-               circleLinks = checkButtonExists[0].buttonCircleData.concat(circleLinks);
-                 updateCircles(circleLinks);
-
-                 //handle text update
-
-                textLinks =checkButtonExists[0].buttonTextData.concat(textLinks);
-                updateText(textLinks);
-
-                return;
-
-           }
-
-           var valueArray = await processedlinks(d.links());
-
-           updatePathLinks = pathLinks.filter(function(item){
-                   return !valueArray.includes(item.target.data.name);
-           });
-
-           //now run the filter to get unfiltered items
-           var clickedPathData = pathLinks.filter(function(item){
-            return valueArray.includes(item.target.data.name);
-            });
-
-
-           updateCircleLinks = circleLinks.filter(function(item){
-                    return !valueArray.includes(item.data.name);
-           });
-
-           var clickedCircleData = circleLinks.filter(function(item){
-                    return valueArray.includes(item.data.name);
-           });
-
-
-           updateTextLinks = textLinks.filter(function(item){
-                    return !valueArray.includes(item.data.name);
-           });
-
-           var clickedTextData = textLinks.filter(function(item){
-                    return valueArray.includes(item.data.name);
-           });
-
-           //now we push the circleData to an array
-           buttonTracker.push({
-               buttonId:buttonId,
-               buttonPathData: clickedPathData,
-               buttonCircleData:clickedCircleData,
-               buttonTextData:clickedTextData
-           })
-
-
-           update(updatePathLinks);
-           updateCircles(updateCircleLinks);
-           updateText(updateTextLinks);
-          async function processedlinks(dlinks) {
-           var valueArray = [];
-
-               return new Promise((resolve, reject)=>{
-                     dlinks.forEach(async(element) =>{
-                          valueArray.push(element.target.data.name);
-                     });
-                     resolve(valueArray);
-               });
-           }
-
-           pathLinks = updatePathLinks;
-           circleLinks = updateCircleLinks;
-           textLinks = updateTextLinks;
-
-    });
+    // .on('mouseover', function(d){
+    //     d3.select(this)
+    //        .attrs({
+    //            'fill':'orange',
+    //        })
+    //        .transition().duration(100).attr('r', 16);
+    // })
+    // .on('mouseout', function(d){
+    //    d3.select(this)
+    //        .attr('fill', (d)=>{
+    //             if(d.children ==undefined){
+    //                 return 'red'
+    //             }
+    //             return 'green'
+    //        })
+    //        .transition().duration(100).attr('r', 12)
+    //
+    // })
+    // .on('click', async function(d){
+    //
+    //        let buttonId = d3.select(this)["_groups"][0][0]["attributes"].id.value;
+    //        mouseX = d3.select(this)["_groups"][0][0]["attributes"].cx.value;
+    //        //check to see if button already exists aka has been clicked
+    //        //if it does, we need to send that data to update function
+    //        //and remove that object
+    //
+    //        let checkButtonExists = buttonTracker.filter(button => button.buttonId == buttonId);
+    //
+    //        if(checkButtonExists[0]!=undefined){
+    //             //also remove this item from button tracker
+    //            buttonTracker = buttonTracker.filter(button => button.buttonId != buttonId);
+    //
+    //            //handle path update
+    //            pathLinks = checkButtonExists[0].buttonPathData.concat(pathLinks);
+    //
+    //            update(pathLinks);
+    //
+    //
+    //             //handle  circle update
+    //            circleLinks = checkButtonExists[0].buttonCircleData.concat(circleLinks);
+    //            drawNode(circleLinks);
+    //
+    //              //handle text update
+    //
+    //             textLinks =checkButtonExists[0].buttonTextData.concat(textLinks);
+    //             updateText(textLinks);
+    //
+    //             return;
+    //
+    //        }
+    //
+    //        var valueArray = await processedlinks(d.links());
+    //
+    //        updatePathLinks = pathLinks.filter(function(item){
+    //                return !valueArray.includes(item.target.data.name);
+    //        });
+    //
+    //        //now run the filter to get unfiltered items
+    //        var clickedPathData = pathLinks.filter(function(item){
+    //         return valueArray.includes(item.target.data.name);
+    //         });
+    //
+    //
+    //        updateCircleLinks = circleLinks.filter(function(item){
+    //                 return !valueArray.includes(item.data.name);
+    //        });
+    //
+    //        var clickedCircleData = circleLinks.filter(function(item){
+    //                 return valueArray.includes(item.data.name);
+    //        });
+    //
+    //
+    //        updateTextLinks = textLinks.filter(function(item){
+    //                 return !valueArray.includes(item.data.name);
+    //        });
+    //
+    //        var clickedTextData = textLinks.filter(function(item){
+    //                 return valueArray.includes(item.data.name);
+    //        });
+    //
+    //        //now we push the circleData to an array
+    //        buttonTracker.push({
+    //            buttonId:buttonId,
+    //            buttonPathData: clickedPathData,
+    //            buttonCircleData:clickedCircleData,
+    //            buttonTextData:clickedTextData
+    //        })
+    //
+    //
+    //        update(updatePathLinks);
+    //     drawNode(updateCircleLinks);
+    //        updateText(updateTextLinks);
+    //       async function processedlinks(dlinks) {
+    //        var valueArray = [];
+    //
+    //            return new Promise((resolve, reject)=>{
+    //                  dlinks.forEach(async(element) =>{
+    //                       valueArray.push(element.target.data.name);
+    //                  });
+    //                  resolve(valueArray);
+    //            });
+    //        }
+    //
+    //        pathLinks = updatePathLinks;
+    //        circleLinks = updateCircleLinks;
+    //        textLinks = updateTextLinks;
+    //
+    // });
 
 
 }
-
-updateCircles(rootNode.descendants());
+    updateText(textLinks);
+    drawNode(rootNode.descendants());
 
 
 function updateText(data){
@@ -283,13 +266,8 @@ function updateText(data){
         }
 
       )
-      .call(text => text.transition().duration(1000).attrs({
-          'x':(d) =>d.y+20,
-          'font-size':15,
-          'fill':'yellow',
-        }));
 }
 
-updateText(textLinks);
+
 
 }
